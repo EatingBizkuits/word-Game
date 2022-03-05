9a45115a4ade9b5cbb4428983523e79f563a6ac7
EatingBizkuits <s10222083@connect.np.edu.sg>
2022-03-06 00:58:29 +0800

    feat: new - border shrink, bug fix - 16grids issue

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,54 @@
-let outer = true; // outer border applied
+let outer = true; // outer border hidden applied
+let allowHover = true;
 
 const words = {
     "round1": [
         ["Elephant", "Giraffe", "Zebra", "Antelope", "Koala", "Tiger", "Monkey", "Penguin", "Reindeer", "Panda", "Dog", "Rhinoceros", "Peacock", "Cow", "Chameleon", "Quokka"],
-        ["Camel", "Dolphin", "Hippopotamus", "Flamingo", "Porcupine", "Goat", "Wolf", "Chicken", "Turtle", "Sloth", "Llama", "Tortoise", "Frog", "Fox", "Cheetah", "Owl", "Gorilla", "Bat", "Squirrel", "Orangutan"]
+        ["Camel", "Dolphin", "Hippo-<br>potamus", "Flamingo", "Porcupine", "Goat", "Wolf", "Chicken", "Turtle", "Sloth", "Llama", "Tortoise", "Frog", "Fox", "Cheetah", "Owl", "Gorilla", "Bat", "Squirrel", "Orangutan"]
     ],
-    "round2": []
+    "round2": [
+
+    ]
 }
 
 $(document).ready(function() {
-    answerInit()
-
-    $(document).on("click", ".gridbox", function(){
+    answerInit();
+    
+    // handle click grid
+    $(document).on("click", ".gridbox", function(){  // click
         if (!($(this).hasClass("outer"))) {
             $(this).addClass("clicked");
         }
         NumberOfChecked()
     });
 
-    $(document).on("click", ".gridbox.clicked", function(){
+    $(document).on("click", ".gridbox.clicked", function(){  // unclick
         $(this).removeClass("clicked");
         NumberOfChecked()
     });
+    // handle click grid end
 
+    // handle hover highlight
+    $(".options li").mouseover(function() {  // hover
+        if (allowHover) {
+            let parentwidth = $(this).width();      
+            $(".underline").width(parentwidth);  
+            $(this).children().removeClass("bar").addClass("in-animate");
+        }
+    });
+
+    $(".options li").mouseout(function() {  // hover off
+        $(this).children().addClass("bar").removeClass("in-animate");
+    });  
+    // handle hover highlight end
+    
+    $(document).on("click", ".options li", function() {
+        let choice = $(this).attr('id');
+        
+        if (choice === "clear") {
+            $(".outer").removeClass("clicked")
+        }
+    });
 });
 
 
@@ -45,17 +71,15 @@ function answerInit(){
 
             selectedList = removeItemInArray(selectedList, chosenText);
             //remove word from list and replace current list with new list (prevent dups)
-            
         }
-        console.log("next")
     }
 }
 
 
 function NumberOfChecked() {  // Shows the outer layer when all inner has been answered
     // remove outer tag only when done
-    let clickedOBJ = document.getElementsByClassName("clicked");
-    
+    let clickedOBJ = $(".clicked:not('.outed.clicked')")
+
     if (clickedOBJ.length >= 16) {
         // if all inner are checked
         outer = false;
@@ -68,17 +92,26 @@ function NumberOfChecked() {  // Shows the outer layer when all inner has been a
     }
 }
 
+
 function outerStatus() { // Replaces Status
-    if (outer) {
+    if (outer) { // to hide outer
         $(".outed").addClass("outer").addClass("animated").removeClass("outed");
-    } else {
-        $(".outer").addClass("outed").removeClass("outer");
+        setTimeout(function() {
+            $(".borderbox").removeClass("enlarge").addClass("shrink");
+        }, 1000)
+
+    } else { // to reveal outer
+        $(".borderbox").addClass("enlarge").removeClass("shrink");
+        $(".outer").addClass("outed").removeClass("outer").removeClass("animated");
+
     }
 }
+
 
 function randomizer(maxLength) {
     return Math.floor((Math.random() * maxLength));
 }
+
 
 function removeItemInArray(array, toRemove){
     let newArray = []
